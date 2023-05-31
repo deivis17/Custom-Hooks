aba782cd3db7ee9f9d5fa8a95a01f1ceabe4f8b0
Deivis Marchán <marchandeivis@gmail.com>
2023-05-31 11:19:55 -0400

    useTodos Added

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,34 @@
+
+export const todoReducer = (initialState = [], action = {})=>{
+
+    switch (action.type) {
+        case '[TODO]: Add Todo':
+            return [...initialState, action.payload];
+
+/*         case '[TODO]: Add New Task':
+            return [...allTodos, action.payload]; */
+        case '[TODO]: Remove Todo':
+            return initialState.filter
+            ( todo => (
+             todo.id !== action.payload  
+            )
+             )
+        case '[TODO] Toggle Done':
+                return initialState.map(todo=>
+                    {
+                        if(action.payload==todo.id){
+                            return{
+                                ...todo,
+                                done: !todo.done,
+                            }
+                        }
+                        return todo;
+                    }
+                    
+                    );
+        default:
+            return initialState;
+    }
+
+
+}
--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from "react";
-import { todoReducer } from "../08-useReducer-2/todoReducer";
+import { todoReducer } from "./todoReducer";
 
 const initialState=[];
 
